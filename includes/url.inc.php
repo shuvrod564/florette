968ae087ccb242f6c4eca727eaaecb815399633f
shuvrod564 <shuvrod564@gmail.com>
2024-02-28 16:47:32 +0600

    url

--- includes/url.inc.php
+++ includes/url.inc.php
@@ -1,7 +1,6 @@
-<?php
-    // $base_url = '192.168.1.4/lifestyle-experts/';
-    // $base_url = 'http://localhost/florette/';
-    $base_url = 'https://florette.embraceindia.co.in/';
+<?php 
+    $base_url = 'http://localhost/florette/';
+    // $base_url = 'https://florette.embraceindia.co.in/';
 
     $site_name="Florette Relocation";
 ?>
